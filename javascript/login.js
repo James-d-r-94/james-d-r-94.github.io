f5c54222fa1937a3e1de2bc1918667aeec9cb0dd
Dodgy-J <51656873+Dodgy-J@users.noreply.github.com>
2021-07-06 16:05:51 +1000

    added login

--- javascript/login.js
+++ javascript/login.js
@@ -1,21 +1,46 @@
 const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+const loginButton = document.getElementById("login-button");
+//const loginErrorMsg = document.getElementById("login-error-msg");
 
 // When the login button is clicked, the following code is executed
 loginButton.addEventListener("click", (e) => {
     // Prevent the default submission of the form
     e.preventDefault();
     // Get the values input by the user in the form fields
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+    //const username = loginForm.username.value;
+    //const password = loginForm.password.value;
+	const username = document.getElementById("uname").value;
+    const password = document.getElementById("psw").value;
+	
+	// const username = loginForm.uname.value;
+    // const password = loginForm.psw.value;
+	
+	
 
-    if (username === "user" && password === "web_dev") {
+    if (username === "admin" && password === "admin") {
         // If the credentials are valid, show an alert box and reload the page
         alert("You have successfully logged in.");
-        location.reload();
+		window.location.href = "RequestForm.html";
+        //location.reload();
     } else {
         // Otherwise, make the login error message show (change its oppacity)
-        loginErrorMsg.style.opacity = 1;
+        //loginErrorMsg.style.opacity = 1;
+		alert("Please input correct username and password");
+		document.getElementById("uname").focus();
     }
 })
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function forgotPassword()
+{
+	alert("User: admin, Password: admin");
+}
